a65acd87c016383ed6c83d33840b406b1d958d25
joao-conde <joaoconde_13@hotmail.com>
2018-05-23 00:23:06 +0100

    toandfro bug

--- sphere-online-judge/toandfro.cpp
+++ sphere-online-judge/toandfro.cpp
@@ -6,31 +6,40 @@
 using namespace std;
 
 /*
-*   Nao funciona mas a ideia é converter o input numa string ou array 1D e usar basicamente algo como SIZE*LINE+COL para ir ler a letra certa
-*   Há claramente um padrao de leitura
-*   tty  -> thisistheeasyoneab - this is the easy one
-    hho     -> ttyohhieneesiaabss
-    ien     -> 2*size - 1  e dps +1 , alternando
-    see
-    iaa         0 + 2*size -1 
-    ssb
-
+*   começa num ponto, le, anda para a direita, le, anda para a esquerda, repete
+*   anda o que sobrar para a direita * 2 + 1 e o que sobrar para a esquerda + 1
+*   dá duas voltas por assim dizer mais a casa de "descer a coluna"
 */
-string decrypt(string cypher, int size){
-    
+string read_chypher_col(string cypher, int size, int col){
     string decrypted = "";
-    int step = 2*size-1, read = 0, idx = -1;
 
-    while(read < cypher.size()){
+    int step_l = col * 2 + 1;
+    int step_r = (size - col - 1)*2 + 1;
+    int col_length = cypher.size() / size, read = 0, idx = col;
+
+    while(read < col_length){
+        decrypted += cypher[idx];
+
+        idx += step_r;
+        decrypted += cypher[idx];
         
-        idx++;
-        decrypted += cypher[idx];
-        idx += step;
-        decrypted += cypher[idx];
+        idx += step_l;
 
         read += 2;
     }
-    
+
+
+    return decrypted;
+}
+
+string decrypt(string cypher, int size){
+    string decrypted = "";
+    int i = 0;
+
+    while(i < size){
+        decrypted += read_chypher_col(cypher, size, i);
+        i++;
+    }
 
     return decrypted;
 }
@@ -48,10 +57,9 @@ int main(){
         
         cin >> cypher;
     
-        cout << decrypt(cypher, size) << endl;
+        cout << decrypt(cypher, size) << "END" << endl;
         
     }
-
 
     return 0;
 }
