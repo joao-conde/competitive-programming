d55590915865a6dfcd4c5672e48740cc40164f9c
joao-conde <joaoconde_13@hotmail.com>
2018-05-31 22:16:01 +0100

    breaking bug

--- sphere-online-judge/breaking.cpp
+++ sphere-online-judge/breaking.cpp
@@ -1,56 +1,60 @@
 //http://www.spoj.com/problems/BREAKING/
 
+#include <math.h>
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
+//TODO: solve this, getting WA
 
+//Eratosthenes' Sieve
+vector<int> generate_primes_upto(int n){
+    vector<int> primes_upto_n; 
+    
+    int sqrt_n = sqrt(n);
+    int not_prime[sqrt_n] = {}; //all inititated as '0' = prime, '1' = not prime
 
-/*
-*   Wrong solution: 
-*   how to get a list of primes up to N
-*   check if they divide N correctly
-*/
-vector<int> get_prime_divisors(int dividend){
-    vector<int> primes;
+    for(int i = 2; i < sqrt_n; i++){
 
-    int prime = 2;
-    while(true){
+        if(not_prime[i] == 1) continue;
 
-        if(dividend % prime == 0){
-            primes.push_back(prime);
-            dividend /= prime;
-            cout << prime << " divides " << dividend;
+        primes_upto_n.push_back(i);
+        for(int j = i; j < sqrt_n; j+=i){
+            not_prime[j] = 1;
         }
 
-        if(dividend == 1)
-            return primes;
-
-        prime++;
     }
-
-    return primes;
-} 
-
-
-void print_primes(vector<int> primes){
-    for(int i = 0; i < primes.size(); i++)
-        cout << primes.at(i) << endl;
+    
+    return primes_upto_n;
 }
 
 
+//global variable => heap
+vector<int> primes;
 
 int main(){
 
     int test_cases, n;
-    cin >> test_cases; cin.ignore();
+    cin >> test_cases; 
+    
+    primes = generate_primes_upto(1000000);
+
     
     for(int i = 0; i < test_cases; i++){
-        cin >> n; cin.ignore();
+        cin >> n;
 
-        cout << "Case " << i+1 << ": "; 
-        print_primes(get_prime_divisors(n));
+        cout << "Case " << i+1 << ":";
+
+        for(int factor: primes){
+            if(n % factor == 0){
+                cout << " ";
+                cout << factor;
+            }
+        }
+
+        cout << endl;           
     }
 
     return 0;
