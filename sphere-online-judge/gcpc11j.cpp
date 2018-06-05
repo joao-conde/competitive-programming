b617b0061b9f1edc6efc861397cf9b552a512961
joao-conde <joaoconde_13@hotmail.com>
2018-06-05 23:28:10 +0100

    getting WA now... jfc

--- sphere-online-judge/gcpc11j.cpp
+++ sphere-online-judge/gcpc11j.cpp
@@ -1,9 +1,10 @@
 //http://www.spoj.com/problems/GCPC11J/
 
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <queue>
-#include <algorithm>
+#include <cmath>
 
 
 using namespace std;
@@ -33,44 +34,35 @@ void bfs(const vector< vector<int> > &g, int source, vector<int>& dist) {
 
 int main(){
 
-    int test_cases, computers, src, dest;
+    int test_cases, computers, src, dest, furthestNode;
+    float networkTTL;
     cin >> test_cases;
 
     for(int i = 0; i < test_cases; i++){
         
         cin >> computers;
-        vector< vector<int> > network(computers);
         
+        vector< vector<int> > network(computers);
+        vector<int> dist(network.size(), INF), dist2(network.size(), INF);
 
         //build network from user input
         for(int j = 0; j < computers - 1; j++){
             cin >> src; cin >> dest;
+
+            //bi-directional edges
             network[src].push_back(dest);
             network[dest].push_back(src);
         }
 
-        
-        int networkTTL = INF;
-          
-        
-        //calculate maximum ttl assuming a router, choose the router assumption with smaller TTL
-        for(int l = 0; l < computers; l++){
-            vector<int> dist(network.size(), INF);
-            bfs(network, l, dist); 
 
-            if(networkTTL > *max_element(dist.begin(), dist.end()))
-                networkTTL = *max_element(dist.begin(), dist.end());
+        //use bfs to calculate distances from first node and discover furthest node from root
+        bfs(network, 0, dist); 
+        furthestNode = *max_element(dist.begin(), dist.end());
 
-            // cout << "---ROUTER ON " << l << "---" << endl; 
-            /*
-            for(int distancia: dist){
-                cout << "DIST: " << distancia << endl;
-            }*/
-            
-        }
-        
-        cout << networkTTL << endl;
-      
+        bfs(network, furthestNode, dist2);
+        networkTTL = *max_element(dist2.begin(),dist2.end());
+
+        cout << ceil(networkTTL/2) << endl;
     }
     
     return 0;
