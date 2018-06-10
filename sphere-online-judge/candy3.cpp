d22e672b8954cb910e326f2dc3702098b023230f
Joao Dias Conde Azevedo <up201503256@i00cs0311.fe.up.pt>
2018-06-10 23:08:54 +0100

    template and candy3 start

---
+++ sphere-online-judge/candy3.cpp
@@ -0,0 +1,30 @@
+//http://www.spoj.com/problems/CANDY3/
+
+#include <bits/stdc++.h> //Includes ALL c++ files needed
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(0); //Input and output become more efficient.
+    cin.tie();
+
+    int test_cases;
+    cin >> test_cases;
+
+    for(int i = 0; i < test_cases; i++){
+
+        long long dummy, children, candies = 0;
+        cin >> children;
+
+        for(int k = 0; k < children; k++){
+            cin >> dummy;
+            candies += dummy;
+        }
+
+        cout << ((candies % children != 0) ? "NO" : "YES") << endl;
+
+    }
+
+    return 0;
+}
---
+++ template.cpp
@@ -0,0 +1,8 @@
+//this includes all needed libraries
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); //input and output become more efficient.
+    cin.tie();
+}
