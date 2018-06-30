7e201a3814898542af9b3be2efcac47cae441302
joao-conde <joaoconde_13@hotmail.com>
2018-06-30 18:01:01 +0100

    spoj WILLITST

---
+++ spoj/willitst.cpp
@@ -0,0 +1,39 @@
+//https://www.spoj.com/problems/WILLITST/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+ * 	If it cycles then it never ends, if not it does.
+ */
+
+bool checkCycle(unsigned long long n){
+	vector<int> nValues;
+
+	while(n > 1){
+
+		if(find(nValues.begin(), nValues.end(), n) != nValues.end())
+			return true;
+
+		nValues.push_back(n);
+
+		if(n % 2 == 0){
+			n = n/2;
+		}
+		else{
+			n = 3*n + 3;
+		}
+	}
+
+	return false;
+}
+
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie();
+
+    unsigned long long n;
+    cin >> n;
+    cout << (checkCycle(n) ? "NIE" : "TAK") << "\n";
+}
