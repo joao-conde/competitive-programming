93f7eef4ba8d39d13007b9bf7cdf1b4d6688aed8
joao-conde <joaoconde_13@hotmail.com>
2018-07-06 20:20:26 +0100

    dynamic prog PARTY spoj start

---
+++ spoj/party.cpp
@@ -0,0 +1,33 @@
+//https://www.spoj.com/problems/PARTY/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* TIL:
+*   1 - ...
+*   2 - ...
+*   3 - ...
+*/
+
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie();
+
+    while(true){
+		int budget, parties;
+		cin >> budget; cin >> parties;
+
+		if(budget == 0 && parties == 0) //end of input
+			break;
+
+		while(parties--){
+			int cost, fun;
+			cin >> cost; cin >> fun;
+		}   
+
+    }
+
+
+
+}
