4a0e7a8661a8b593ffe3bbad354c35497e92a012
joao-conde <joaoconde_13@hotmail.com>
2018-07-10 16:33:20 +0100

    821 PAGE HOPPING UVA

--- uva/821-page-hopping.cpp
+++ uva/821-page-hopping.cpp
@@ -1,17 +1,17 @@
-//src: https://www.geeksforgeeks.org/fast-io-for-competitive-programming/
+//https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=762
 
 #include <bits/stdc++.h>
 using namespace std;
 
-/* TIL:
-*   1 - ...
-*   2 - ...
-*   3 - ...
-*/
-
-
 #define INF 1000000000
 
+/*
+ * Breadth-First-Search -> O(E + V)
+ *
+ * Calculates the shortest path between a source node and all other nodes in a graph 
+ * defined as an adjacency list with all edges having the same cost (1).
+ *
+ */
 void bfs(const vector< vector<int> > &g, int source, vector<int>& dist) {
 	queue<int> q;
 	dist[source] = 0;
@@ -27,63 +27,92 @@ void bfs(const vector< vector<int> > &g, int source, vector<int>& dist) {
 	}
 }
 
+/*
+ * Computes the average shortest path of a node to all other ones
+ */
+double computeAverageShortestPath(const vector<vector<int>> &g, int src){
 
-vector<vector<int>> buildGraph(vector<pair<int,int>> edges, int size){
+	double avg = 0, undefined = 0;
+	vector<int> dist(g.size(), INF);
 
-	vector<vector<int>> graph(size);
+	bfs(g, src, dist);
 
-	for(int i = 0; i < edges.size(); i++){
-		graph[edges[i].first].push_back(edges[i].second);
+	for(int i = 0; i < dist.size(); i++){
+
+		if(i == src) continue;
+
+		if(dist.at(i) == INF){
+			undefined++;
+			continue;
+		} 
+		
+		avg += dist[i];
+
+	}
+
+	avg /= (g.size() - undefined - 1);
+
+	return avg;
+}
+
+
+/*
+ *  Builds a graph based on a edges vector assuming each 2 elements are in the format of src->dest
+ */
+vector<vector<int>> buildGraph(const vector<int> &edges){
+
+	int gSize = *max_element(edges.begin(), edges.end());
+
+	vector<vector<int>> graph(gSize);
+
+	for(int i = 0; i < edges.size(); i+=2){
+		graph[edges[i]-1].push_back(edges[i+1]-1);
 	}
 
 	return graph;
 }
 
+
 int main() {
     ios::sync_with_stdio(0); 
     cin.tie();
 
-    int src, dest;    
+    int src, dest, n = 1;    
     while(true){
 
     	cin >> src; cin >> dest;
 
     	if(src == 0 && dest == 0) break;
 
-    	int size = max(src,dest);
-    	vector<pair<int,int>> edges;
-    	edges.push_back(make_pair(src,dest));
+    	vector<int> edges;
+    	edges.push_back(src); 
+    	edges.push_back(dest);
 
     	while(true){
     		cin >> src; cin >> dest;
 
     		if(src == 0 && dest == 0) break;
-
-    		edges.push_back(make_pair(src,dest));
-    		size = max(size, max(src,dest));
+    		
+    		edges.push_back(src); 
+			edges.push_back(dest);
     	}
 
-    	cout << "HERE\n";
+    	vector<vector<int>> graph = buildGraph(edges);
 
-    	size -= 1;
-    	vector<vector<int>> graph = buildGraph(edges, size);
 
-    	int sum = 0, combinations = 0;
+    	double avg = 0, undefined = 0;
+    	for(int i = 0; i < graph.size(); i++){
+    		if(graph[i].size() != 0)
+    			avg += computeAverageShortestPath(graph, i);
+    		else 
+    			undefined++;
+    	}
 
-		for(int i = 0; i < size; i++){
-			vector<int> dist(size, INF);
-			bfs(graph, i, dist);
+    	avg /= (graph.size() - undefined);
 
-			for(int j = 0; j < dist.size(); j++){
-				sum += dist[j];
-				combinations++;
-			}
-		}
-
-    	//print testcase results
-    	cout << "AVG: " << (double)sum / double(combinations) << "\n";
+    	cout << setprecision(3) << fixed << "Case " << n << ": average length between pages = " << avg << " clicks\n";
+    	n++;
     }
-
 
 }
 
