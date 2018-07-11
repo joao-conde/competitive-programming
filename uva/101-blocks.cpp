455015d7ca97404abc593c419578d6e823e51ed2
joao-conde <joaoconde_13@hotmail.com>
2018-07-11 01:12:21 +0100

    chosen next UVA problem

---
+++ uva/101-blocks.cpp
@@ -0,0 +1,16 @@
+//https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=3&page=show_problem&problem=37
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* TIL:
+*   1 - ...
+*   2 - ...
+*   3 - ...
+*/
+
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie();
+}
