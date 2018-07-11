bb8947389f1ca73664ade235c1b858319b701995
joao-conde <joaoconde_13@hotmail.com>
2018-07-11 16:14:33 +0100

    initial solution structure

--- uva/101-blocks.cpp
+++ uva/101-blocks.cpp
@@ -9,8 +9,39 @@ using namespace std;
 *   3 - ...
 */
 
+void printWorld(vector<stack<int>> bworld){
+
+	cout << "\n---BLOCK WORLD---";
+	for(int i = 0; i < bworld.size(); i++){
+		cout << "\n" << i << ":";
+
+		stack<int> auxStack = bworld.at(i);
+		while(!auxStack.empty()){
+			cout << " " << auxStack.top();
+			auxStack.pop();
+		}
+	}
+	cout << "\n";
+
+}
 
 int main() {
+    
     ios::sync_with_stdio(0); 
     cin.tie();
+
+    int nBlocks;
+    cin >> nBlocks;
+
+    vector<stack<int>> bworld;
+
+    for(int i = 0; i < nBlocks; i++){
+    	stack<int> stacki;
+    	stacki.push(i);
+    	bworld.push_back(stacki);
+    } 
+
+    printWorld(bworld);
+
+
 }
