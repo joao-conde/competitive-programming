eda05d4b8ee925098534294625b50bb0fe70b18e
João Conde <up201503256@fe.up.pt>
2018-09-27 00:51:38 +0100

    digit dp, must read about it

---
+++ spoj/cpcrc1c.cpp
@@ -0,0 +1,27 @@
+//https://www.spoj.com/problems/CPCRC1C/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+/* TIL:
+*   1 - DIGIT DP: https://www.geeksforgeeks.org/digit-dp-introduction/
+*   2 - ...
+*   3 - ...
+*/
+
+
+int main() {
+   
+    ios::sync_with_stdio(0); 
+    cin.tie();
+
+    while(true){
+
+        int a, b; cin >> a >> b;
+        if(a == -1 && b == -1) break;
+
+        
+
+    }
+}
