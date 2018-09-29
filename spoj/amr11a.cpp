c46ed6c2ff1ccaa4c470cf347486f7b63299b639
João Conde <up201503256@fe.up.pt>
2018-09-29 21:09:05 +0100

    MIUP QUALIFICATION CONTEST SOLUTIONS

--- spoj/amr11a.cpp
+++ spoj/amr11a.cpp
@@ -3,8 +3,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define INF 100000
-
+#define INF 10000000
+//TODO YET
 int main() {
 
     ios::sync_with_stdio(0); 
@@ -15,7 +15,8 @@ int main() {
     while(testCases--){
 
         int rows, cols; cin >> rows >> cols;
-        int mgrid[rows][cols];
+
+        int mgrid[rows][cols], dp[rows][cols]; //pair of current strength and lowest strength ever recorded through path
 
         //magic grid from input
         for(int r = 0; r < rows; r++){
@@ -25,33 +26,62 @@ int main() {
         }
 
         //harry starts at (0, 0) in my 0 index notation
-        mgrid[0][0] = mgrid[rows][cols] = 0;
+        mgrid[0][0] = mgrid[rows][cols] = dp[0][0] = 0;
 
-        //GOAL: find maximum "strength" path but tracking HP lowest
-        int lowestStrength = INF; //lowest HP Harry Potter hit, minimum strength is the one that covers that
-
+        //GOAL: find maximum "strength" path but tracking HP lowest strength
         //first column
         for(int r = 1; r < rows; r++){
-            mgrid[r][0] += mgrid[r-1][0];
+            pair<int, int> nextPair = mgrid[r][0];
+            nextPair.first += mgrid[r-1][0].first;
+
+            if(nextPair.first < mgrid[r][0].second)
+                nextPair.second = nextPair.first;
+            else
+                nextPair.second = mgrid[r][0].second; 
         }
 
         //first row
         for(int c = 1; c < cols; c++){
-            mgrid[0][c] += mgrid[0][c-1];
+            pair<int, int> nextPair = mgrid[0][c]; 
+            nextPair.first += mgrid[0][c-1].first;
+
+            if(nextPair.first < mgrid[0][c].second)
+                nextPair.second = nextPair.first;
+            else
+                nextPair.second = mgrid[0][c].second; 
         }
 
         //bottom-up approach
         for(int r = 1; r < rows; r++){
             for(int c = 1; c < cols; c++){
-                int maxStrength = max(mgrid[r-1][c], mgrid[r][c-1]);
-                lowestStrength = min(lowestStrength, maxStrength);
-                mgrid[r][c] += maxStrength;
+
+                pair<int, int> maxLowerStrength;
+
+                if(mgrid[r-1][c].second < mgrid[r][c-1].second)
+                    maxLowerStrength = mgrid[r-1][c];
+                else 
+                    maxLowerStrength = mgrid[r][c-1];
+
+                maxLowerStrength.first += mgrid[r][c].first;
+
+                if(maxLowerStrength.first < maxLowerStrength.second)
+                    maxLowerStrength.second = maxLowerStrength.first;
+
+                mgrid[r][c] = maxLowerStrength;
             }
         }
 
-        if(lowestStrength <= 0) cout << -lowestStrength + 1 << "\n";
+        // if(lowestStrength <= 0) cout << -lowestStrength + 1 << "\n";
         
-        else cout << "1" << "\n";
+        // else cout << "1" << "\n";
+
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++)
+                cout << " " << mgrid[i][j].second;
+
+            cout << endl;
+        }
 
     }
 
