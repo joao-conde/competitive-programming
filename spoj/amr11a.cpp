442925fafd100daa41d319a404c7dfde44f3e355
João Conde <up201503256@fe.up.pt>
2018-09-30 23:30:52 +0100

    amr11a and shrooms both GREAT problems combining DP and BS

--- spoj/amr11a.cpp
+++ spoj/amr11a.cpp
@@ -3,8 +3,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define INF 10000000
-//TODO YET
+#define INF 1000000000
+
+
+/* TIL:
+*   1 - Binary search doing wonders again :D
+*   2 - DP allows us to compute the most OPTIMAL PATH if at each step you can make the OPTIMAL CHOICE (not the case here)
+*/
+
+int dp[501][501], mgrid[501][501];
+
+bool isSolution(int val, int rows, int cols){
+
+    //lets assume we can start with val strength on HP
+    mgrid[0][0] = dp[0][0] = val;
+
+    //first column
+    for(int r = 0; r < rows; r++){
+        dp[r][0] = dp[r-1][0] + mgrid[r][0];
+
+        //if we 'die' (hit 0 or less strength) then this path does not satisfy us
+        //so we give it -INF and later on opt for high strength paths
+        if(dp[r][0] <= 0) dp[r][0] = -INF;
+    }
+
+
+    //first row (IDENTICAL TO COLUMN)
+    for(int c = 0; c < cols; c++){
+        dp[0][c] = dp[0][c-1] + mgrid[0][c];
+        if(dp[0][c] <= 0) dp[0][c] = -INF;
+    }
+
+    //bottom-up approach
+    for(int r = 1; r < rows; r++){
+        for(int c = 1; c < cols; c++){
+            //looking for maximum strength path
+            dp[r][c] = max(dp[r-1][c], dp[r][c-1]) + mgrid[r][c];
+            if(dp[r][c] <= 0) dp[r][c] = -INF;
+        }
+    }
+
+
+    //val is a solution if in the end we have more than 0 strength
+    return dp[rows-1][cols-1] > 0;
+}
+
+
 int main() {
 
     ios::sync_with_stdio(0); 
@@ -14,74 +58,34 @@ int main() {
 
     while(testCases--){
 
-        int rows, cols; cin >> rows >> cols;
-
-        int mgrid[rows][cols], dp[rows][cols];
+        int rows, cols, transverseEntireGrid = 0; 
+        cin >> rows >> cols;
 
         //magic grid from input
         for(int r = 0; r < rows; r++){
             for(int c = 0; c < cols; c++){
                 cin >> mgrid[r][c];
-                dp[r][c] = -INF;
-            }
-        }
-
-        //harry starts at (0, 0) in my 0 index notation
-        mgrid[0][0] = mgrid[rows-1][cols-1] = 0;
-
-        //GOAL: find maximum minimum strenght path
-        //first column
-        for(int r = 1; r < rows; r++){
-            mgrid[r][0] = mgrid[r-1][0] - mgrid[r][0];
-        }
-
-        //first row
-        for(int c = 1; c < cols; c++){
-            mgrid[0][c] = mgrid[0][c-1] - mgrid[0][c];
-        }
-
-        //bottom-up approach maximizing strength path
-        for(int r = 1; r < rows; r++){
-            for(int c = 1; c < cols; c++){
-                mgrid[r][c] = max(mgrid[r-1][c], mgrid[r][c-1]) - mgrid[r][c]; 
-            }
-        }
-
-        //bottom-up approach
-        for(int r = 1; r < rows; r++){
-            for(int c = 1; c < cols; c++){
-                mgrid[r][c] = max(mgrid[r-1][c], mgrid[r][c-1]) - mgrid[r][c]; 
+                transverseEntireGrid += abs(mgrid[r][c]); 
             }
         }
 
 
-        //bottom-up approach
-        for(int r = 1; r < rows; r++){
-            for(int c = 1; c < cols; c++){
-                dp[r][c] = max(max(mgrid[r-1][c], mgrid[r][c-1]), dp[r][c]);
+        //binary search: trying possible starting values
+        int lb = 1, ub = transverseEntireGrid, mid;
+        while(ub >= lb){
+
+            mid = (lb + ub) / 2;
+
+            if(isSolution(mid, rows, cols)){
+                ub = mid - 1;
             }
+            else{
+                lb = mid + 1;
+            }
+
         }
 
-        // if(lowestStrength <= 0) cout << -lowestStrength + 1 << "\n";
-        
-        // else cout << "1" << "\n";
-
-        cout << "MGRID" << endl;
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++)
-                cout << " " << mgrid[i][j];
-
-            cout << endl;
-        }
-
-
-        cout << "\nDP" << endl;
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++)
-                cout << " " << dp[i][j];
-
-            cout << endl;
-        }
+        cout << lb << "\n";
 
     }
 
