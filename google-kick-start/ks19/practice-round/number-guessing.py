3f778c1f6976de14eb8a0acac3acf58355cb816e
unknown <joaodiasconde@gmail.com>
2020-03-08 19:30:43 +0000

    kick start 2019 practice round ex1

---
+++ google-kick-start/ks19/practice-round/number-guessing.py
@@ -0,0 +1,20 @@
+def guess(lb, up):
+  # binary search
+  mid = lb + (up - lb)//2
+  print(mid) # guess
+  answer = input()
+  if answer == "TOO_BIG":
+    guess(lb, mid - 1)
+  elif answer == "TOO_SMALL":
+    guess(mid + 1, up)
+  else: return
+
+def solve():
+  test_cases = int(input())
+  for _ in range(test_cases):
+    lb, up = map(int, input().split())
+    _ = int(input())
+    guess(lb + 1, up)
+
+if __name__ == "__main__":
+  solve()
