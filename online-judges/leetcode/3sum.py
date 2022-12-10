45f55c1257beecc4b9479a862a1a67b29a122cba
Joao Conde <joaodiasconde@gmail.com>
2022-12-10 00:11:23 +0000

    feat: leetcode twopointers 3sum

---
+++ online-judges/leetcode/3sum.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/3sum/
+
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+
+        triplets = set()
+        for i in range(len(nums) - 2):
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                triplet = (nums[i], nums[j], nums[k])
+                total = sum(triplet)
+
+                if total == 0:
+                    triplets.add(triplet)
+                    j += 1
+                    k -= 1
+                elif total < 0:
+                    j += 1
+                else:
+                    k -= 1
+
+        return list([list(t) for t in triplets])
+
+
+# Tests
+solver = Solution()
+assert [-1, 0, 1] in solver.threeSum([-1, 0, 1, 2, -1, -4])
+assert [-1, -1, 2] in solver.threeSum([-1, 0, 1, 2, -1, -4])
+assert solver.threeSum([0, 1, 1]) == []
+assert solver.threeSum([0, 0, 0]) == [[0, 0, 0]]
