e78be35a6554dd2a6c07d974a51d256fa8d81330
Joao Conde <joaodiasconde@gmail.com>
2022-12-10 00:26:37 +0000

    feat: sum 3 closest, same logic as 3sum, two pointers decreases by n factor time complexity

---
+++ online-judges/leetcode/3sum-closest.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/3sum-closest/
+
+from typing import List
+
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+
+        closest = float("-inf")
+        for i in range(len(nums) - 2):
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+                if total == target:
+                    return total
+
+                if abs(total - target) < abs(closest - target):
+                    closest = total
+
+                if total > target:
+                    k -= 1
+                else:
+                    j += 1
+
+        return closest
+
+
+# Tests
+solver = Solution()
+assert solver.threeSumClosest([-1, 2, 1, -4], 1) == 2
+assert solver.threeSumClosest([0, 0, 0], 1) == 0
