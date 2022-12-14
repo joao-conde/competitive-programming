0196e3546b303d0364c179fa64a335f6e7e199a3
Joao Conde <joaodiasconde@gmail.com>
2022-12-14 18:58:56 +0000

    refactor: better bin search

--- online-judges/leetcode/binary-search.py
+++ online-judges/leetcode/binary-search.py
@@ -6,17 +6,14 @@ from typing import List
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         lb, ub = 0, len(nums) - 1
-
-        mid = lb + (ub - lb) // 2
         while lb <= ub:
+            mid = lb + (ub - lb) // 2
             if nums[mid] < target:
                 lb = mid + 1
             elif nums[mid] > target:
                 ub = mid - 1
             else:
                 return mid
-
-            mid = lb + (ub - lb) // 2
         return -1
 
 
