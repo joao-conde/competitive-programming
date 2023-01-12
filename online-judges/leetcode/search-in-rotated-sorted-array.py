5b452c6e2ce7e337c3a0e62811cd927e8b09bd9a
Joao Conde <joaodiasconde@gmail.com>
2023-01-12 17:14:56 +0000

    fix: search in rotated sorted array

--- online-judges/leetcode/search-in-rotated-sorted-array.py
+++ online-judges/leetcode/search-in-rotated-sorted-array.py
@@ -4,34 +4,30 @@ from typing import List
 
 
 class Solution:
-    def find_pivot(self, nums: List[int]) -> int:
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            if nums[left] <= nums[right]:
-                return left
-            left = left + 1
-        return -1
-
-    def bin_search(self, nums: List[int], left: int, right: int, target: int) -> int:
-        while left < right:
-            mid = left + (right - left) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid
-        return -1
-
     def search(self, nums: List[int], target: int) -> int:
-        pivot_i = self.find_pivot(nums)
-        res = self.bin_search(nums, 0, pivot_i, target)
-        res = res if res != -1 else self.bin_search(nums, pivot_i, len(nums), target)
-        return res
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if target == nums[mid]:
+                return mid
+
+            if nums[left] <= nums[mid]:
+                if target > nums[mid] or target < nums[left]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                if target < nums[mid] or target > nums[right]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return -1
 
 
 # Tests
 solver = Solution()
+assert solver.search([0, 1, 2, 3, 4, 5, 6], 5) == 5
 assert solver.search([4, 5, 6, 7, 0, 1, 2], 0) == 4
 assert solver.search([4, 5, 6, 7, 0, 1, 2], 3) == -1
 assert solver.search([1], 0) == -1
