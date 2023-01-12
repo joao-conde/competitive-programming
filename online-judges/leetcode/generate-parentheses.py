13c7cf7c014ecb1d25542890b8dfb353201452a4
Joao Conde <joaodiasconde@gmail.com>
2023-01-12 15:54:49 +0000

    feat: generate parenthesis DP usage

---
+++ online-judges/leetcode/generate-parentheses.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/generate-parentheses/
+
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def helper(n: int, open: int, closed: int, state: str) -> List[str]:
+            if open + closed == 2 * n:
+                return [state]
+
+            combinations = []
+            if open < n:
+                combinations.extend(helper(n, open + 1, closed, state + "("))
+
+            if closed < n and closed < open:
+                combinations.extend(helper(n, open, closed + 1, state + ")"))
+
+            return combinations
+
+        return helper(n, 1, 0, "(")
+
+
+# Tests
+solver = Solution()
+assert solver.generateParenthesis(3) == [
+    "((()))",
+    "(()())",
+    "(())()",
+    "()(())",
+    "()()()",
+]
+assert solver.generateParenthesis(2) == ["(())", "()()"]
+assert solver.generateParenthesis(1) == ["()"]
