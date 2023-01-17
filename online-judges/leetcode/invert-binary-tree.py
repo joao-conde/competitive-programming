1f5fb07f90a7659e2d1d35809737dea026107a1c
Joao Conde <joaodiasconde@gmail.com>
2023-01-17 16:04:56 +0000

    feat: invert binary tree

---
+++ online-judges/leetcode/invert-binary-tree.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/invert-binary-tree/
+
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root == None:
+            return None
+
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
