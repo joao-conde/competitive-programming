d62d0e0239eedf0f3f98546998ca891d4696337d
Joao Conde <joaodiasconde@gmail.com>
2023-01-27 17:27:07 +0000

    feat: house robber circular DP

---
+++ online-judges/leetcode/house-robber-ii.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/house-robber-ii/
+
+from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        def _rob(ns, cache):
+            if len(ns) == 0:
+                return 0
+
+            key = len(ns)
+            if key in cache:
+                return cache[key]
+
+            cache[key] = max(ns[0] + _rob(ns[2:], cache), _rob(ns[1:], cache))
+            return cache[key]
+
+        return max(_rob(nums[:-1], dict()), _rob(nums[1:], dict()))
+
+
+# Tests
+solver = Solution()
+assert solver.rob([2, 3, 2]) == 3
+assert solver.rob([1, 2, 3, 1]) == 4
+assert solver.rob([1, 2, 3]) == 3
