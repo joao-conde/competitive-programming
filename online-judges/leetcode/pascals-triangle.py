29bd341f36fb8d25bd0f556e3dea423e8adabaf3
Joao Conde <joaodiasconde@gmail.com>
2023-03-22 15:26:34 +0000

    feat: pascals triangle

---
+++ online-judges/leetcode/pascals-triangle.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/pascals-triangle/
+
+from typing import List
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        rows = []
+        for r in range(numRows):
+            row = [1] * (r + 1)
+            for i in range(1, r):
+                row[i] = rows[-1][i - 1] + rows[-1][i]
+            rows.append(row)
+        return rows
+
+
+# Tests
+solver = Solution()
+assert solver.generate(5) == [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+assert solver.generate(1) == [[1]]
