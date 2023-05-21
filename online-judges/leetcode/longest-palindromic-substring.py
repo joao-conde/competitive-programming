0444a8a69ab8799d22e4c6d5f5756df3e9263842
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2023-05-21 18:15:56 +0100

    feat: longest palindromic substring

--- online-judges/leetcode/longest-palindromic-substring.py
+++ online-judges/leetcode/longest-palindromic-substring.py
@@ -2,28 +2,27 @@
 
 
 class Solution:
-    def expand(self, s: str, l: int, r: int) -> tuple[int, int]:
-        x, y = l, r
-        while x >= 0 and y < len(s) and s[x] == s[y]:
-            x -= 1
-            y += 1
-        return (x, y)
-
     def longestPalindrome(self, s: str) -> str:
         start, end = 0, 0
         for i in range(len(s)):
-            (x1, y1) = self.expand(s, i, i)
-            (x2, y2) = self.expand(s, i, i + 1)
+            l1, r1 = self.expand(s, i, i)
+            l2, r2 = self.expand(s, i, i + 1)
 
-            if y1 - x1 > end - start:
-                start = x1
-                end = y1
+            if r1 - l1 > end - start:
+                start = l1
+                end = r1
 
-            if y2 - x2 > end - start:
-                start = x2
-                end = y2
+            if r2 - l2 > end - start:
+                start = l2
+                end = r2
 
         return s[start + 1 : end]
+
+    def expand(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        return (l, r)
 
 
 # Tests
