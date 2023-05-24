d8fd3b621df367247b1f20db66c283dcec424f69
Joao Conde <joaodiasconde@gmail.com>
2023-05-24 16:09:38 +0100

    fix: remove dead

--- online-judges/codeforces/e.py
+++ online-judges/codeforces/e.py
@@ -1,22 +1,20 @@
-def decode(s, i, cache):
-    if i >= len(s):
-        return 1
-
-    if i in cache:
-        return cache[i]
-
-    ways = 1
-    for j in range(i, len(s) - 1):
-        if s[j] == "u" and s[j + 1] == "u":
-            ways += decode(s, j + 2, cache) % (10**9 + 7)
-        elif s[j] == "n" and s[j + 1] == "n":
-            ways += decode(s, j + 2, cache) % (10**9 + 7)
-        elif s[j] == "m" or s[j] == "w" or s[j + 1] == "m" or s[j + 1] == "w":
-            return 0
-
-    cache[i] = ways % (10**9 + 7)
-    return cache[i]
-
-
 s = input()
-print(decode(s, 0, dict()))
+modulo = 10**9 + 7
+
+i = 1
+ways = 1
+while i < len(s) - 1:
+    if s[i] == "m" or s[i] == "w":
+        ways = 0
+        break
+
+    if s[i - 1] == "u" and s[i] == "u":
+        ways = 2 * ways
+
+    if s[i - 1] == "n" and s[i] == "n":
+        ways = 2 * ways
+
+    ways %= modulo
+    i += 1
+
+print(ways)
