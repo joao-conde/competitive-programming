0d2d7375f0603142febab69707496d3b5b985054
Joao Conde <joaodiasconde@gmail.com>
2023-07-25 12:43:39 +0100

    feat: string groups

--- others/string-groups.py
+++ others/string-groups.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 
 
-def string_groups(strings):
+def string_groups_1(strings):
     groups = defaultdict(lambda: set())
 
     for s in strings:
@@ -11,8 +11,31 @@ def string_groups(strings):
     return [list(g) for g in groups.values()]
 
 
+def string_groups_2(strings):
+    groups = defaultdict(lambda: set())
+
+    for s in strings:
+        counts = defaultdict(lambda: 0)
+        for c in s:
+            counts[c] += 1
+        groups[tuple(counts)].add(s)
+
+    return [list(g) for g in groups.values()]
+
+
 # [["241", "124", "412"], ["524", "425"], ["324"], ["2141"]]
-string_groups(
+string_groups_1(
+    [
+        "124",
+        "412",
+        "425",
+        "241",
+        "524",
+        "324",
+        "2141",
+    ]
+)
+string_groups_2(
     [
         "124",
         "412",
@@ -25,7 +48,18 @@ string_groups(
 )
 
 # [["cba", "bca", "abc"], ["c"], ["cc"], ["dc"], ["cdd"]]
-string_groups(
+string_groups_1(
+    [
+        "abc",
+        "bca",
+        "cba",
+        "c",
+        "cc",
+        "dc",
+        "cdd",
+    ]
+)
+string_groups_2(
     [
         "abc",
         "bca",
